8409224e77bb9a906234f6e32febfa6dba49dbec
NghiaDepTry <ngocnghia2504@gmail.com>
2026-04-01 22:19:27 +0700

    Counting inversion using Merge Sort & Vector

--- Data_Structure/Trees/CF/CountingInversion.cpp
+++ Data_Structure/Trees/CF/CountingInversion.cpp
@@ -1,58 +1,45 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-void copy(int a[], int b[], int start, int end)
+int mergesort(vector<int> &v, int l, int r)
 {
-    for (int i = 0; i < end - start + 1; ++i)
-        b[i] = a[i + start];
-}
-int mergesort(int a[], int n, int l, int r)
-{
-    int ans = 0;
     if (l >= r)
         return 0;
+    int ans = 0;
 
     int m = l + (r - l) / 2;
-    ans += mergesort(a, n, l, m);
-    ans += mergesort(a, n, m + 1, r);
+    ans += mergesort(v, l, m);
+    ans += mergesort(v, m + 1, r);
 
-    int len1 = m - l + 1, len2 = r - m;
-    int x[len1], y[len2];
-    copy(a, x, l, m);
-    copy(a, y, m + 1, r);
+    vector<int> x(v.begin() + l, v.begin() + m + 1);
+    vector<int> y(v.begin() + m + 1, v.begin() + r + 1);
 
     int i = 0, j = 0;
-    while (i < len1 && j < len2)
+    while (i < x.size() && j < y.size())
     {
         if (x[i] <= y[j])
-            a[l++] = x[i++];
+            v[l++] = x[i++];
         else
         {
-            ans += len1 - i;
-            a[l++] = y[j++];
+            ans += x.size() - i;
+            v[l++] = y[j++];
         }
     }
-    while (i < len1)
-    {
-        a[l++] = x[i++];
-    }
-    while (j < len2)
-    {
-        a[l++] = y[j++];
-    }
+
+    while (i < x.size())
+        v[l++] = x[i++];
+    while (j < y.size())
+        v[l++] = y[j++];
 
     return ans;
 }
 int main()
 {
-    int a[] = {5, 4, 3, 2, 1};
-    int Size = sizeof(a) / sizeof(a[0]);
-    cout << mergesort(a, Size, 0, Size - 1) << "\n";
+    vector<int> v{5, 4, 3, 2, 1};
+    cout << mergesort(v, 0, v.size() - 1) << "\n";
 
-    for (int i = 0; i < Size; ++i)
-    {
-        cout << a[i] << " ";
-    }
+    for (int i = 0; i < v.size(); ++i)
+        cout << v[i] << " ";
     return 0;
 }
