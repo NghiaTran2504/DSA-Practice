4850d056db79723de84fbcd6a880676b9eb2d767
NghiaDepTry <ngocnghia2504@gmail.com>
2026-04-04 10:42:35 +0700

    Couting inversions - 3 indexes (Hell to me)

--- Data_Structure/Trees/CF/Fenwick_tree.cpp
+++ Data_Structure/Trees/CF/Fenwick_tree.cpp
@@ -1,131 +1,128 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 #define ll long long
 
-void mergesort(int[], int, int);
-int lowerbound(int[], int, int);
-struct Fenwick
-{
-    ll *BIT;
-    ll *a;
-    int n;
+void heapify (ll, int, int);
+void heapsort (ll, int);
+void Update (ll*, int, int);
+ll Query (int);
 
-    Fenwick()
-    {
-        cin >> n;
-        BIT = new ll[n + 5]();
-        a = new ll[n + 5]();
+void heapify (ll a[], int n, int i){
+    int node = i;
+    while (1){
+        int left = 2 * node + 1;
+        int right = 2 * node + 2;
+        int largest = node;
 
-        for (int i = 1; i <= n; ++i)
-        {
-            cin >> a[i];
-        }
+        if (left < n && a[left] > a[largest])
+            largest = left;
+        if (right < n && a[right] > a[largest])
+            largest = right;
+
+        if (largest == node) return;
+        swap (a[largest], a[node]);
+        node = largest;
     }
-
-    void update(int pos)
-    {
-        while (pos <= n)
-        {
-            BIT[pos] += 1;
-            pos += pos & -pos;
-        }
-    }
-
-    ll query(int val)
-    {
-        ll ans = 0;
-        while (val)
-        {
-            ans += BIT[val];
-            val -= val & -val;
-        }
-        return ans;
-    }
-
-    void clear()
-    {
-        delete[] a;
-        delete[] BIT;
-    }
-};
-
-void mergesort(int a[], int left, int right)
-{
-    if (left >= right)
-        return;
-    ll mid = left + (right - left) / 2;
-    mergesort(a, left, mid);
-    mergesort(a, mid + 1, right);
-
-    int len1 = mid - left + 1;
-    int len2 = right - mid;
-    int i = 0, j = 0;
-
-    int x[len1], y[len2];
-    for (int i = 0; i < len1; ++i)
-        x[i] = a[i + left];
-    for (int i = 0; i < len2; ++i)
-        y[i] = a[mid + 1 + i];
-
-    while (i < len1 && j < len2)
-    {
-        if (x[i] <= y[j])
-            a[left++] = x[i++];
-        else
-            a[left++] = y[j++];
-    }
-
-    while (i < len1)
-        a[left++] = x[i++];
-    while (j < len2)
-        a[left++] = y[j++];
 }
 
-int lowerbound(int a[], int n, int val)
+void heapsort (ll a[], int n)
 {
-    int left = 1, right = n + 1;
-    while (left < right)
-    {
+    for (int i =  n / 2 - 1; i >= 0; i--)
+        heapify (a, n, i);
+    
+    for (int i = n - 1; i >= 1; --i){
+        swap (a[0], a[i]);
+        heapify (a, i, 0);
+    }
+}
+
+int lowerbound (ll a[], int n, ll val){
+    int left = 0, right = n - 1;
+    int res = n;
+    while (left <= right){
         int mid = left + (right - left) / 2;
-        if (a[mid] >= val)
-            right = mid;
-        else
-            left = mid + 1;
+        if (a[mid] >= val){
+            res = mid;
+            right = mid - 1;
+        }
+        else left = mid + 1;
     }
 
-    return right;
+    return res;
+}
+
+void Update (ll *bit, int pos, int n){
+    while (pos <= n){
+        bit[pos]++;
+        pos += pos & -pos;
+    }
+}
+
+ll Query (ll *bit, int pos){
+    ll ans = 0;
+    while (pos){
+        ans += bit[pos];
+        pos -= pos & -pos;
+    }
+    return ans;
+}
+
+void solve(){
+    int n; cin >> n;
+    ll* a = new ll [n+5]();
+    ll* bit = new ll[n+5]();
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+
+    ll* b = new ll [n+5]();
+    for (int i = 0; i < n; ++i){
+        b[i] = a[i + 1];
+    }
+    heapsort (b, n);
+
+    //Compress numbers
+    for (int i = 1; i <= n; ++i){
+        a[i] = lowerbound (b, n, a[i]) + 1;
+    }
+    //###############
+
+    ll* suffix = new ll [n + 5]();
+    ll* prefix = new ll [n + 5]();
+
+    //Prefix
+    for (int i = 1; i <= n; ++i){
+        prefix[i] = i - 1 - Query (bit, a[i]);
+        Update (bit, a[i], n);
+    }
+
+    memset (bit, 0, sizeof(ll) * (n + 5));
+
+    for (int i = n; i >= 1; --i){
+        suffix[i] = Query (bit, a[i] - 1);
+        Update (bit, a[i], n);
+    }
+
+    // for (int i = 1; i <= n; ++i ) cout << prefix[i] << " ";
+    // cout << "\n";
+    // for (int i = 1; i <=n; ++i) cout << suffix[i] << " ";
+
+    ll ans = 0;
+    for (int i = 1; i <= n; ++i){
+       // cout << prefix[i] << " " << suffix[i] << "\n";
+        ans += prefix[i] * suffix[i];
+    }
+
+    cout << ans << " ";
+    delete[] a;
+    delete[] b;
+    delete[] bit;
 }
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    solve();
 
-    Fenwick T;
-
-    int b[T.n + 5];
-    for (int i = 1; i <= T.n; ++i)
-    {
-        b[i] = T.a[i];
-    }
-
-    mergesort(b, 1, T.n);
-
-    // Number compress:
-    for (int i = 1; i <= T.n; ++i)
-    {
-        T.a[i] = lowerbound(b, T.n, T.a[i]);
-    }
-
-    ll cnt = 0;
-    for (int i = 1; i <= T.n; ++i)
-    {
-        ll kq = T.query(T.a[i]);
-        cnt += i - kq - 1;
-        T.update(T.a[i]);
-    }
-
-    cout << cnt << " ";
-    T.clear();
     return 0;
 }
