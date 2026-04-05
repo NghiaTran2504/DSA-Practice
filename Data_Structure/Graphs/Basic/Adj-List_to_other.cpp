7cb1d8a955fd48356b4fef3044c6603715f56fb3
NghiaDepTry <ngocnghia2504@gmail.com>
2026-04-05 10:36:50 +0700

    Change location

---
+++ Data_Structure/Graphs/Basic/Adj-List_to_other.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <string>
+
+using namespace std;
+void Adj_mat (int n){
+    cin >> n;
+    for (int i = 1; i <= n; ++i){
+        string s;
+        stringstream ss(s);
+        string res;
+        
+    }
+}
+int main()
+{
+    int n;
+    return 0;
+}
