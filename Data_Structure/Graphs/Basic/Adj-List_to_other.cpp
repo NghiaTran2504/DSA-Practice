44c6c91fc124a4d0e16f3c6bbff6be4dc628439c
NghiaDepTry <ngocnghia2504@gmail.com>
2026-04-05 10:59:22 +0700

    Bieu dien do thi tu danh sach ke sang other

--- Data_Structure/Graphs/Basic/Adj-List_to_other.cpp
+++ Data_Structure/Graphs/Basic/Adj-List_to_other.cpp
@@ -4,17 +4,46 @@
 #include <string>
 
 using namespace std;
+
 void Adj_mat (int n){
     cin >> n;
+    cin.ignore();
+
+    int mat[n+5][n+5] = {};
     for (int i = 1; i <= n; ++i){
         string s;
+        getline (cin, s);
         stringstream ss(s);
         string res;
-        
+        while (ss >> res){
+            mat[i][stoi(res)] = 1;
+        }        
+    }
+
+    for (int i = 1; i <=n; ++i){
+        for (int j = 1; j <=n; ++j){
+            cout << mat[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void EdgeList (int n){
+    cin >> n;
+    cin.ignore();
+    for (int i = 1; i <= n; ++i){
+        string s;
+        getline(cin,s);
+        stringstream ss (s);
+        string res;
+        while (ss >> res){
+            if (stoi (res) >= i) cout << i << " "  << stoi(res) << "\n";
+        }
     }
 }
 int main()
 {
     int n;
+    EdgeList(n);
     return 0;
 }
