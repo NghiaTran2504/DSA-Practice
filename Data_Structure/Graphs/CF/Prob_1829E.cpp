dcc11c6caa6e424e96ef1962cd301950be378cd2
NghiaDepTry <ngocnghia2504@gmail.com>
2026-04-06 16:47:18 +0700

    Solve using DFS

---
+++ Data_Structure/Graphs/CF/Prob_1829E.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+using namespace std;
+
+struct Pair{
+    int x;
+    int y;
+};
+
+struct Node{
+    Pair coordinate;
+    Node* next;
+    Node (int x, int y){
+        coordinate.x = x;
+        coordinate.y = y;
+        next = NULL;
+    }
+};
+void addNode (Node* &head, Pair val){
+    Node* newNode = new Node (val.x, val.y);
+    newNode -> next = head;
+    head = newNode;
+}
+
+void deleteNode (Node* &head){
+    if (head == NULL) return;
+    Node* tmp = head;
+    head = head -> next;
+    delete tmp;
+}
+
+struct Stack{
+    Node* top;
+    int size;
+    Stack(){
+        top = NULL;
+        size = 0;
+    }
+    void push(Pair val){
+        addNode (top, val);
+        size++;
+    }
+    void pop(){
+        deleteNode (top);
+        size--;
+    }
+    bool empty(){
+        return size == 0;
+    }
+    void print(){
+        Node* curr = top;
+        cout << "Stack hien tai la: ";
+        while (curr != NULL){
+            cout << "{" << curr ->coordinate.x << ", " << curr -> coordinate.y << "}, ";
+            curr = curr -> next;
+        }
+        cout << "\n";
+    }
+};
+
+long long maxx (long long a, long long b){
+    if (a >= b) return a;
+    else return b;
+}
+
+void dfs (int dx[], int dy[], long long &vol, int row, int col, int** mat, int n, int m){
+    Stack st;
+    Pair p;
+    p.x = row; p.y = col;
+    st.push (p);
+
+    while (!st.empty()){
+        int row = st.top -> coordinate.x;
+        int col = st.top -> coordinate.y;
+        vol += mat[row][col];
+        mat[row][col] = 0;
+        st.pop();
+
+        for (int i = 0; i < 4; ++i){
+                Pair tmp;
+                tmp.x = row + dx[i];
+                tmp.y = col + dy[i];
+                if (tmp.x >= 1 && tmp.x <= n && tmp.y >= 1 && tmp.y <= m  && mat[tmp.x][tmp.y] > 0){
+                    st.push (tmp);
+                }
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int dx[] = {-1, 1, 0, 0};
+    int dy[] = {0, 0, -1, 1};
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        int** mat = new int* [n + 5];
+        for (int i = 1; i <= n; ++i){
+            mat[i] = new int[m + 5]{};
+        }
+
+        for (int i = 1; i <= n; ++i){
+            for (int j = 1; j <= m; ++j)
+                cin >> mat[i][j];
+        }
+
+        long long max_vol = 0;
+        for (int i = 1; i <= n; ++i){
+            for (int j = 1; j <= m; ++j){
+                long long val = 0;
+                if (mat[i][j] > 0) dfs (dx, dy, val, i, j, mat, n, m);
+                max_vol = maxx (max_vol, val);
+            }
+        }
+
+        cout << max_vol << "\n";
+
+        for (int i = 1; i <= n; ++i){
+            delete[] mat[i];
+        }
+        delete[] mat;
+    }
+}
